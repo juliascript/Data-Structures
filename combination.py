ccb0131cd9b54e870a8e6e579a779aa7a44d04f6
Julia <juliageist23@gmail.com>
2017-01-27 13:03:17 -0800

    using n/k * n-k

--- combination.py
+++ combination.py
@@ -1,8 +1,17 @@
+from recursion import factorial
 import permutation
 
 def combination(data, k):
-	permutedData = permutation(data)
-	selection = []
-	for i in range(k):
-		selection.append(i)
-	return selection
+
+	if k > n:
+		return 0
+	else: 
+		n = len(data)
+		comb = (factorial(n) / ((factorial(k) * (factorial(n-k))) )
+		return comb
+
+	# permutedData = permutation(data)
+	# selection = []
+	# for i in range(k):
+	# 	selection.append(i)
+	# return selection
